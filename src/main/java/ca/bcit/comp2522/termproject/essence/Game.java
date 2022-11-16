711c81c8c4d62250c3237d67f87eb654a8513993
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-11-15 23:28:43 -0800

    feat: create base game class

---
+++ src/main/java/ca/bcit/comp2522/termproject/essence/Game.java
@@ -0,0 +1,19 @@
+package ca.bcit.comp2522.termproject.essence;
+
+/**
+ * Manages and updates game components.
+ *
+ * @author Benjamin Chiang
+ * @version 0.1.0
+ */
+public class Game {
+
+  /**
+   * Creates a new instance of the game.
+   *
+   * @param window   reference to the containing window
+   * @param tickrate frequency of game updates
+   */
+  public Game(final Window window, final int tickrate) {
+  }
+}
