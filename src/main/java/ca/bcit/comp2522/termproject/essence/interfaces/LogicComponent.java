c3f4c846db34f50be0907f45f8d3cc5e624f279b
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-11-17 00:35:01 -0800

    feat: create logic component interface

---
+++ src/main/java/ca/bcit/comp2522/termproject/essence/interfaces/LogicComponent.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2522.termproject.essence.interfaces;
+
+/**
+ * A component containing logic that is required to be updated.
+ *
+ * @author Benjamin Chiang
+ * @version 0.1.0
+ */
+public interface LogicComponent {
+  /**
+   * Updates the component.
+   */
+  void update();
+}
