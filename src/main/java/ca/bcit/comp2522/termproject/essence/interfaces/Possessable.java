fcfc9a6a246ff97e14a90761c3b7838b15749199
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-11-18 20:39:58 -0800

    feat: create a possessable interface

---
+++ src/main/java/ca/bcit/comp2522/termproject/essence/interfaces/Possessable.java
@@ -0,0 +1,21 @@
+package ca.bcit.comp2522.termproject.essence.interfaces;
+
+/**
+ * An object that is possessable by a controller.
+ *
+ * @author Benjamin Chiang
+ * @version 0.1.0
+ */
+public interface Possessable {
+  /**
+   * Sets the controller.
+   *
+   * @param controller controller to set
+   */
+  void possess(Controller controller);
+
+  /**
+   * Removes the current controller.
+   */
+  void unpossess();
+}
