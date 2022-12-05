2ccd2ad51caf1bb46622bc43cb20acc482f25255
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-12-04 21:35:33 -0800

    refactor: remove fallback constructors

--- src/main/java/ca/bcit/comp2522/termproject/essence/sprites/PlayerSprite.java
+++ src/main/java/ca/bcit/comp2522/termproject/essence/sprites/PlayerSprite.java
@@ -1,7 +1,6 @@
 package ca.bcit.comp2522.termproject.essence.sprites;
 
 import ca.bcit.comp2522.termproject.essence.Sprite;
-import ca.bcit.comp2522.termproject.essence.Vec2D;
 
 /**
  * A sprite for the player character.
@@ -15,35 +14,5 @@ public class PlayerSprite extends Sprite {
    */
   public PlayerSprite() {
     super(Sprite.SPRITE_PLAYER);
-  }
-
-  /**
-   * Creates a sprite using a given image.
-   *
-   * @param position sprite position
-   */
-  public PlayerSprite(final Vec2D position) {
-    super(Sprite.SPRITE_PLAYER, position);
-  }
-
-  /**
-   * Creates a sprite using a given image.
-   *
-   * @param position sprite position
-   * @param scale    sprite scale factor
-   */
-  public PlayerSprite(final Vec2D position, final double scale) {
-    super(Sprite.SPRITE_PLAYER, position, scale);
-  }
-
-  /**
-   * Creates a sprite using a given image.
-   *
-   * @param position sprite position
-   * @param scale    sprite scale factor
-   * @param radians  sprite rotation in radians
-   */
-  public PlayerSprite(final Vec2D position, final double scale, final double radians) {
-    super(Sprite.SPRITE_PLAYER, position, scale, radians);
   }
 }
